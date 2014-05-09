5b7dfa5b38cfb5a6c4a0f398d1b4f7bc57b6a9e9
gim6014 <gim6014@hanmail.net>
2014-05-09 15:46:35 +0900

    오류 대부분 수정 남은 건 사진 보이기 숨기기 검색기능 설정기능 수정

--- StoryboardNavigation/NewsArticleViewController.m
+++ StoryboardNavigation/NewsArticleViewController.m
@@ -43,20 +43,36 @@
     saveNewsArr=[[NSMutableArray alloc]init];
     appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     
+    
     linkstring=[NSMutableString stringWithString:[passData2 description]];
    
     newsdetail=[htmlparsing sethtml:linkstring];
     photostring = [htmlparsing getphotourl];
     
+   
     
     i_height=0;
     i_width=0;
-    text_top_margin = 150;
+    text_top_margin = 0;
     
 }
 -(void)viewWillAppear:(BOOL)animated{
     
-    //타이틀 설정 및 텍스트 삽입
+    for (NSString *line in [[newsdetail substringFromIndex:4] componentsSeparatedByString:@"\n"]) {
+        if (![line isEqualToString:@"\n"]) {
+            [newsdetailarr addObject:line];
+        }
+        
+    }
+    
+    
+    mainScrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(0,0,320,520)];
+    mainScrollView.showsVerticalScrollIndicator=YES;
+    mainScrollView.scrollEnabled=YES;
+    mainScrollView.userInteractionEnabled=YES;
+    mainScrollView.isAccessibilityElement=NO;
+    [mainScrollView sizeToFit];
+
     titleview =[[UILabel alloc]initWithFrame:CGRectMake(10.0f, 65.0f, 290.0f, 150.0f)];
     [titleview setText:[passData description]];
     titleview.lineBreakMode = YES;
@@ -66,59 +82,22 @@
     titleview.numberOfLines = 0;
     titleview.lineBreakMode = YES;
     titleview.adjustsFontSizeToFitWidth = YES;
-
-    
-
-    
-    mainScrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(0,0,320,520)];
-    mainScrollView.showsVerticalScrollIndicator=YES;
-    mainScrollView.scrollEnabled=YES;
-    mainScrollView.userInteractionEnabled=YES;
-    mainScrollView.isAccessibilityElement=NO;
-    //mainScrollView.contentSize=CGSizeMake(320,2000);
-    [mainScrollView sizeToFit];
-
-    
-    
     [titleview setFont:[UIFont systemFontOfSize:appDelegate.fontS]];
-    
-    
-    
-    
-    
-        for (NSString *line in [[newsdetail substringFromIndex:6] componentsSeparatedByString:@"\n"]) {
-            if (![line isEqualToString:@"\n"]) {
-                [newsdetailarr addObject:line];
-            }
-            
-        }
+    [titleview sizeToFit];
+    [mainScrollView addSubview:titleview];
     
     if (photostring!=NULL) {
-
-        webview = [[UIWebView alloc] initWithFrame:CGRectMake(30.0f, 190.0f, 250.0f, 190.0f)];
-        text_top_margin = 260;
         
-        UIScrollView *scrollView = nil;
-        if ([webview respondsToSelector:@selector(scrollView)]) { //iOS 5+
-            scrollView = webview.scrollView;
-        } else { //iOS 4-
-            for(UIView *view in webview.subviews){
-                if([view isKindOfClass:[UIScrollView class] ]){
-                    scrollView = (UIScrollView *) view;
-                    break;
-                }
-            }
-        }
-        scrollView.scrollEnabled = NO;
-        scrollView.bounces = NO;
-        scrollView.backgroundColor=[UIColor clearColor];
-    
+        NSLog(@"titleview.frame.size.height=%f",titleview.frame.size.height);
+        webview = [[UIWebView alloc] initWithFrame:CGRectMake(30.0f, titleview.frame.size.height+75.0f, 250.0f, 190.0f)];
         NSURL *myURL = [NSURL URLWithString:photostring];
         NSURLRequest *myURLReq = [NSURLRequest requestWithURL:myURL];
         [webview loadRequest:myURLReq];
         [webview setScalesPageToFit:YES];
-        }
         
+    }
+
+    /*
         for (int i=0; i<[newsdetailarr count]; i++) {
             
             //NSLog(@"height=%d,width=%d",i_height,i_width);
@@ -160,15 +139,83 @@
         }
     
         mainScrollView.contentSize=CGSizeMake(320,i_height+310);
+        */
+    [webview sizeToFit];
+    [mainScrollView addSubview:webview];
+    [self.view addSubview:mainScrollView];
+    
+}
+
+
+-(void) viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
+    if ([webview sizeThatFits:CGSizeMake(webview.frame.size.width, FLT_MAX)].height > webview.frame.size.height) {
         
-        [mainScrollView addSubview:webview];
+        CGRect frame = webview.frame;
+        frame.size.height = 1;
+        webview.frame = frame;
+        CGSize fittingSize = [webview sizeThatFits:CGSizeMake(webview.frame.size.width, FLT_MAX)];
+        frame.size = fittingSize;
+        webview.frame = frame;
         
+    }
+    
+    if ([htmlparsing getphotourl]==NULL) {
+         text_top_margin=titleview.frame.size.height+60;
+    }else {
+        text_top_margin=webview.frame.size.height+40;
+    }
+    //text_top_margin=webview.frame.size.height+150;
+    NSLog(@"text_top_margin=%d",text_top_margin);
+    NSLog(@"webview.frame.size.height=%f",webview.frame.size.height);
+
+    for (int i=0; i<[newsdetailarr count]; i++) {
+        
+        //NSLog(@"height=%d,width=%d",i_height,i_width);
+        
+        imagetextview=[[UILabel alloc]initWithFrame:CGRectMake(20, text_top_margin+i_height+40, 280, i_height)];
+        [imagetextview setFont:[UIFont systemFontOfSize:15.0f]];
+        [imagetextview setFont:[UIFont boldSystemFontOfSize:appDelegate.fontS]];
+        //imagetextview.editable = NO;
+        //imagetextview.selectable= YES;
+        imagetextview.userInteractionEnabled=YES;
+        imagetextview.accessibilityTraits=UIAccessibilityTraitNotEnabled;
+        imagetextview.multipleTouchEnabled=YES;
+        imagetextview.opaque=NO;
+        imagetextview.tag = 10;
+        imagetextview.numberOfLines = 0;
+        
+        
+        //[imagetextview setScrollEnabled:YES];
+        NSMutableString * buffer =[NSMutableString stringWithString:newsdetailarr[i]];
+        [buffer appendString:@"\n"];
+        
+        [imagetextview setText:buffer];
+        imagetextview.lineBreakMode = YES;
+        
+        [imagetextview sizeToFit];
+        //[imagetextview setScrollEnabled:NO];
+        
+        CGSize textViewSize = [imagetextview sizeThatFits:CGSizeMake(imagetextview.frame.size.width, FLT_MAX)];
+        i_height+=textViewSize.height;
+        i_width+=textViewSize.width;
+        
+        if (textViewSize.width==10) {
+            imagetextview.isAccessibilityElement=NO;
+        }
+        
+        
+        [mainScrollView addSubview:imagetextview];
+        
+    }
+    
+    mainScrollView.contentSize=CGSizeMake(320,i_height+310);
+    
+    
    
-        [titleview sizeToFit];
-        [mainScrollView addSubview:titleview];
-        
-       
-        [self.view addSubview:mainScrollView];
+
     
 }
 
@@ -199,5 +246,6 @@
     
     
 }
+
 
 @end
