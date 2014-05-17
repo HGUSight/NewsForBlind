bfa7ad6d5ae682afdb34796889fe120f78795868
gim6014 <gim6014@hanmail.net>
2014-05-17 17:00:58 +0900

    원상복구함

--- StoryboardNavigation/TextFieldViewController.m
+++ StoryboardNavigation/TextFieldViewController.m
@@ -62,6 +62,7 @@
 - (IBAction) textFieldDoneEditing:(id) sender // 텍스트 필드 입력 종료 액션
 {
 	[sender resignFirstResponder]; // 센더 (텍스트 필드)에게 최초 응답자 자리에서 물러나라는 메시지이다.
+    
 }
 
 - (IBAction) Keyboardhidding:(id) sender // 텍스트 필드 입력 종료 액션
